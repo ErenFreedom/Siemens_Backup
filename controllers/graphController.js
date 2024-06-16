62b21f29ed836a02059ad066fc26fe39a139997f
kartik <kartik@kali.kartik>
2024-06-16 08:31:39 +0100

    Added backend files

--- controllers/graphController.js
+++ controllers/graphController.js
@@ -1,6 +1,5 @@
 const db = require('../config/db');
 
-// Helper function to get the starting timestamp based on the filter
 const getStartTime = (filter) => {
     const now = new Date();
     switch (filter) {
@@ -21,7 +20,6 @@ const getStartTime = (filter) => {
     }
 };
 
-// Function to get data and metrics based on the filter
 const getDataAndMetrics = (table, filter, callback) => {
     const startTime = getStartTime(filter);
     const query = `SELECT * FROM ${table} WHERE timestamp >= ? ORDER BY timestamp ASC`;
@@ -42,8 +40,6 @@ const getDataAndMetrics = (table, filter, callback) => {
         const average = (sum / values.length).toFixed(2);
         const max = Math.max(...values).toFixed(2);
         const min = Math.min(...values).toFixed(2);
-
-        // Additional professional metrics
         const range = (max - min).toFixed(2);
         const variance = (values.reduce((a, b) => a + Math.pow(b - average, 2), 0) / values.length).toFixed(2);
         const stddev = Math.sqrt(variance).toFixed(2);
@@ -65,30 +61,6 @@ exports.getTempData = (req, res) => {
     const { filter } = req.params;
     getDataAndMetrics('temp', filter, (err, result) => {
         if (err) return res.status(500).send('Error fetching temperature data');
-        res.json(result);
-    });
-};
-
-exports.getPressureData = (req, res) => {
-    const { filter } = req.params;
-    getDataAndMetrics('pressure', filter, (err, result) => {
-        if (err) return res.status(500).send('Error fetching pressure data');
-        res.json(result);
-    });
-};
-
-exports.getHumidityData = (req, res) => {
-    const { filter } = req.params;
-    getDataAndMetrics('humidity', filter, (err, result) => {
-        if (err) return res.status(500).send('Error fetching humidity data');
-        res.json(result);
-    });
-};
-
-exports.getRhData = (req, res) => {
-    const { filter } = req.params;
-    getDataAndMetrics('rh', filter, (err, result) => {
-        if (err) return res.status(500).send('Error fetching RH data');
         res.json(result);
     });
 };
