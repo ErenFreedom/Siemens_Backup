ce3b73e2f912524edf8a7085b3ce31894cf4d192
kartik <kartik@kali.kartik>
2024-06-18 08:47:59 +0100

    Added controller files

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const authenticateToken = require('../middlewares/authenticateToken');
 const accountController = require('../controllers/accountController');
 
 // Generate OTP
-router.post('/account/generate-otp', authenticateToken, accountController.generateOTP);
+router.post('/account/generate-otp', accountController.generateOTP);
 
 // Verify OTP
-router.post('/account/verify-otp', authenticateToken, [
+router.post('/account/verify-otp', [
     check('otp', 'OTP is required').notEmpty()
 ], accountController.verifyOTP);
 
 // Edit Account
-router.put('/account/edit', authenticateToken, [
+router.put('/account/edit', [
     check('email', 'Email is required').isEmail(),
     check('username', 'Username is required').notEmpty()
 ], accountController.editAccount);
 
 // Change Password
-router.put('/account/change-password', authenticateToken, [
+router.put('/account/change-password', [
     check('oldPassword', 'Old password is required').notEmpty(),
     check('newPassword', 'New password is required').isLength({ min: 6 })
 ], accountController.changePassword);
 
 // Delete Account
-router.delete('/account/delete', authenticateToken, accountController.deleteAccount);
+router.delete('/account/delete', accountController.deleteAccount);
 
 module.exports = router;
