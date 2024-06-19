c7837ddd55b907df1b5e96a24c0ae541eb4b1120
kartik <kartik@kali.kartik>
2024-06-19 11:57:41 +0100

    Added component files

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import dataReducer from './dataReducer';
-import notificationReducer from './notificationReducer';
 
 const rootReducer = combineReducers({
   data: dataReducer,
-  notifications: notificationReducer,
 });
 
 export default rootReducer;
